68d8dd3b3489ad37d6907f64bebb2c56224019b7
Pasha Anisa <aneesha.dhani@gmail.com>
2016-09-07 09:23:34 -0800

    Adding editText consist of name, KTP and phone number.

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3025/formlamarankerja/MainActivity.java
@@ -0,0 +1,43 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl3025.formlamarankerja;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.EditText;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etKTP, etPhone;
+    Button butOK;
+    TextView tvHasil;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etKTP = (EditText) findViewById(R.id.editTextKTP);
+        etPhone = (EditText) findViewById(R.id.editTextPhone);
+        butOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        butOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = etNama.getText().toString();
+                String KTP = etKTP.getText().toString();
+                String phone = etPhone.getText().toString();
+
+                    tvHasil.setText("Your Name: " + nama + "."+"\n"+"KTP: " + KTP + "."+"\n"+"Phone number: " + phone + ".");
+
+            }
+        });
+
+    }
+}
